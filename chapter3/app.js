db3d27289432716b701d1191d46b2f51e1712543
Sauleha14Shakeel <saulehashakeel14@gmail.com>
2026-03-03 09:32:58 -0800

    Done

---
+++ chapter3/app.js
@@ -0,0 +1,25 @@
+//Question:1
+let age = 18;
+alert("I am " + age + " years old");
+//Question:2
+let N = 15;
+alert("You have visited this site " + N + " times");
+//Question:3
+let birthYear = 2007;
+
+document.write("My birth Year is " + birthYear);
+document.write("<br/>");
+document.write("Datatype of my declare variable is " + typeof birthYear);
+//Question:4
+let visitorName = "John Doe";
+let productTitle = "T-shirt(s)";
+let quantity = "5";
+
+document.write("<br/>");
+document.write(
+  visitorName +
+    " ordered " +
+    quantity +" "+
+    productTitle +
+    " on XYZ Clothing store"
+);
